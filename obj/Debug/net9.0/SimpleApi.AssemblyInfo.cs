1a2614e347079f28469104a52e022c358facf438
ya-anwar22 <ayaanwar.se@gmail.com>
2026-06-09 11:48:27 +0300

    setep 2

--- obj/Debug/net9.0/SimpleApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/SimpleApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+494d5c22b428bdb34bf0c577ec883fa67de183df")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
